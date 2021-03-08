6829cb0a84f7fc62236dc0f409038a3e2d79b98a
fikret <61146337+FikretCansel@users.noreply.github.com>
2021-03-08 13:35:01 +0300

    edited performance and transaction aspects

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect:MethodInterception
+    {
+        public TransactionScopeAspect(IInvocation invocation)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    scope.Complete();
+                }
+                catch (Exception)
+                {
+                    scope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
